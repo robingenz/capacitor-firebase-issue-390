393dfca8192dabeb6f23a4fad53986c7342bda67
Robin Genz <mail@robingenz.dev>
2023-06-26 15:02:04 +0200

    Add example

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, DoCheck {
 
   constructor() {}
+
+  ngOnInit(): void {
+    initializeApp(environment.firebase);
+    const auth = getAuth();
+    console.log(auth.currentUser)
+  }
+
+  ngDoCheck(): void {
+    console.log('docheck');
+  }
 
 }
